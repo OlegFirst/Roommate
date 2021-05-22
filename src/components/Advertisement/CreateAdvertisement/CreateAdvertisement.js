f8245eb1d060696329d3d2bdad99091716bd8c97
Oleg <OlegFIRST@ukr.net>
2021-05-22 20:00:46 +0300

    AdvertisementCreate

--- src/components/Advertisement/CreateAdvertisement/CreateAdvertisement.js
+++ src/components/Advertisement/CreateAdvertisement/CreateAdvertisement.js
@@ -7,16 +7,33 @@ import ImageLoader from '../../_commonComponents/ImageLoader/ImageLoader';
 import { createAdvetritesement } from '../../../func/createAdvetritesement';
 
 const CreateAdvertisement = () => {
+	const refLocation = useRef(null);
+	const refStreet = useRef(null);
+	const refNumberOfRooms = useRef(null);
+	const refNumberPeople = useRef(null);
+	const refPrice = useRef(null);
+	const refDescription = useRef(null);
+	// TO DO: Photos
+	
 	const refImageLoader = useRef(null);
 	const [imageLoaderShow, setImageLoaderShow] = useState(false);
 	
+	// location: 'Kyiv Улица',
+			// price: 5000,
+			// sleepingPlaces: 1,
+			// description: 'Text',
+	
 	const createAdvetritesementHandler = async () => {
-		const result = await createAdvetritesement({
-			location: 'Kyiv Улица',
-			price: 5000,
-			sleepingPlaces: 1,
-			description: 'Text',
-		});
+		const data = {
+			location: refLocation.current.value + ' ' + refStreet.current.value,
+			price: refPrice.current.value,
+			sleepingPlaces: refNumberOfRooms.current.value,
+			description: refDescription.current.value
+		};
+		
+		//console.log(data)
+		
+		const result = await createAdvetritesement(data);
 		console.dir(result);
 	};
 
@@ -37,32 +54,32 @@ const CreateAdvertisement = () => {
 			<ul className="create-advertisement__text text">
 				<li className="text__item">
 					<span>Location:</span>
-					<FormControl placeholder="Input city/village" />
+					<FormControl placeholder="Input city/village" ref={refLocation} />
 				</li>
 
 				<li className="text__item">
 					<span>Street:</span>
-					<FormControl placeholder="Input full address" />
+					<FormControl placeholder="Input full address" ref={refStreet} />
 				</li>
 
 				<li className="text__item">
 					<span>Number of rooms:</span>
-					<FormControl />
+					<FormControl ref={refNumberOfRooms} type="number" />
 				</li>
 
 				<li className="text__item">
 					<span>Number of people to live:</span>
-					<FormControl />
+					<FormControl ref={refNumberPeople} type="number" />
 				</li>
 
 				<li className="text__item">
 					<span>Price:</span>
-					<FormControl />
+					<FormControl ref={refPrice} type="number" />
 				</li>
 
 				<li className="text__item">
 					<span>Decription:</span>
-					<FormControl as="textarea" />
+					<FormControl as="textarea" ref={refDescription} />
 				</li>
 			</ul>
 
