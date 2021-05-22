136e010cb62807bbf4d526e84ed241c191f2c00b
gcoda <gcoding@gmail.com>
2021-05-22 19:47:12 +0300

    showing first advert details

--- src/func/account.js
+++ src/func/account.js
@@ -11,6 +11,18 @@ export const serverGetAccount = () =>
 			data: null,
 		}));
 
+export const serverGetMyAdverts = () =>
+	axios
+		.get('account/listing')
+		.then((res) => ({
+			isSuccess: true,
+			data: res.data.data,
+		}))
+		.catch((err) => ({
+			isSuccess: false,
+			data: null,
+		}));
+
 export const serverPutAccount = (data) =>
 	axios
 		.put('account/me', data)
