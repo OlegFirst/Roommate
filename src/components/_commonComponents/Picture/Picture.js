05364c7059a8cd17a677b37c18f032a781a35c54
gcoda <gcoding@gmail.com>
2021-05-23 14:48:29 +0300

    fix: React: Redundant alt attribute. Screen-readers already announce `img` tags as an image. You don’t need to use the words `image`, `photo,` or `picture` (or any specified custom words) in the alt prop

--- src/components/_commonComponents/Picture/Picture.js
+++ src/components/_commonComponents/Picture/Picture.js
@@ -1,7 +1,7 @@
 const Picture = ({ url }) => {
 	return (
 		<div className="picture">
-			<img className="picture__image" src={url} alt="Picture" />
+			<img className="picture__image" src={url} alt="" />
 		</div>
 	);
 };
